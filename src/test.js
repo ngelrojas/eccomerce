40e6736cacd1606ba0acce9702c30bde5db41b8d
ngel rojas p <aomarrojasp@gmail.com>
2020-01-16 16:40:57 -0300

    auth in firebase

---
+++ src/test.js
@@ -0,0 +1,7 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+const firestore = firebase.firestore();
+
+firestore.collection('users').doc('cOZUHbPw8lGD19xnN3bm').collection('cartItems').doc('HGafxRyWKWEt1pC8Hjtg')
+firestore.doc('/users/')
