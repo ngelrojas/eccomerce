be1b3242a8d6c0274184d0a85ffb6480cc592eae
ngel rojas p <aomarrojasp@gmail.com>
2020-02-05 16:07:21 -0300

    add select

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,25 +1,23 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import CustomButtom from '../custom-button/custom-button.component.jsx';
-import CartItem from '../cart-item/cart-item.component.jsx';
-import './cart-dropdown.styles.scss';
+import React from 'react'
+import {connect} from 'react-redux'
+import CustomButtom from '../custom-button/custom-button.component.jsx'
+import CartItem from '../cart-item/cart-item.component.jsx'
+import {selectCartItems} from '../../redux/cart/cart.selectors.js'
+import './cart-dropdown.styles.scss'
 
-
-const CartDropdown = ({ cartItems }) => (
-    <div className="cart-dropdown">
-        <div className="cart-items">
-            {
-                cartItems.map(cartItem =>(
-                    <CartItem key={cartItem.id} item={cartItem} />
-                )) 
-            }     
-        </div>
-        <CustomButtom>GO TO CHECKOUT</CustomButtom>
+const CartDropdown = ({cartItems}) => (
+  <div className="cart-dropdown">
+    <div className="cart-items">
+      {cartItems.map(cartItem => (
+        <CartItem key={cartItem.id} item={cartItem} />
+      ))}
     </div>
-);
+    <CustomButtom>GO TO CHECKOUT</CustomButtom>
+  </div>
+)
 
-const mapStateToProps = ({cart: {cartItems}}) =>({
-    cartItems
+const mapStateToProps = state => ({
+  cartItems: selectCartItems(state),
 })
 
-export default connect(mapStateToProps)(CartDropdown);
+export default connect(mapStateToProps)(CartDropdown)
