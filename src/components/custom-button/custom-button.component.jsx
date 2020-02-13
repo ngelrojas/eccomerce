1415ab66da49cd8ce40df739516a771b1dd0f39e
ngel rojas p <aomarrojasp@gmail.com>
2020-02-13 19:08:24 -0300

    new branch

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,12 +1,9 @@
-import React from 'react';
+import React from 'react'
+import {CustomButtonContainer} from './custom-button.styles.jsx'
+// import './custom-button.styles.scss'
 
-import './custom-button.styles.scss';
+const CustomButton = ({children, ...props}) => (
+  <CustomButtonContainer {...props}>{children}</CustomButtonContainer>
+)
 
-
-const CustomButton = ({children, isGoogleSignIn, inverted, ...otherProps}) => (
-    <button className={`${inverted ? 'inverted': ''} ${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`} {...otherProps}>
-        {children} 
-    </button>
-);
-
-export default CustomButton;
+export default CustomButton
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,40 +1,40 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import {createStructuredSelector} from 'reselect'
-import {Link} from 'react-router-dom'
+// import {Link} from 'react-router-dom'
 import {auth} from '../../firebase/firebase.utils.js'
 import {ReactComponent as Logo} from '../../assets/crown.svg'
 import CartIcon from '../../components/cart-icon/cart-icon.component.jsx'
 import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component.jsx'
 import {selectCurrentUser} from '../../redux/user/user.selectors.js'
 import {selectCartHiden} from '../../redux/cart/cart.selectors.js'
-import './header.styles.scss'
+import {
+  HeaderContainer,
+  LogoContainer,
+  OptionsContainer,
+  OptionLink,
+} from './header.styles.jsx'
+// import './header.styles.scss'
 
 const Header = ({currentUser, hidden}) => (
-  <div className="header">
-    <Link className="logo-container" to="/">
+  <HeaderContainer>
+    <LogoContainer to="/">
       <Logo className="logo" />
-    </Link>
-    <div className="options">
-      <Link className="option" to="/shop">
-        SHOP
-      </Link>
-      <Link className="option" to="/contact">
-        CONTACT
-      </Link>
+    </LogoContainer>
+    <OptionsContainer>
+      <OptionLink to="/shop">SHOP</OptionLink>
+      <OptionLink to="/contact">CONTACT</OptionLink>
       {currentUser ? (
-        <div className="option" onClick={() => auth.signOut()}>
+        <OptionLink as="div" onClick={() => auth.signOut()}>
           SIGN OUT
-        </div>
+        </OptionLink>
       ) : (
-        <Link className="option" to="/signin">
-          SIGN IN
-        </Link>
+        <OptionLink to="/signin">SIGN IN</OptionLink>
       )}
       <CartIcon />
-    </div>
+    </OptionsContainer>
     {hidden ? null : <CartDropdown />}
-  </div>
+  </HeaderContainer>
 )
 
 const mapStateToProps = createStructuredSelector({
